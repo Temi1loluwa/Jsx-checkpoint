172095219c7c1c94f5e3e2d3eae4cf7e6608b436
Temi1loluwa <temiloluwagoodness9@gmail.com>
2022-03-31 22:15:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ border: "solid 1px black", width: "100vw" }}>
+        <img src={"/img 1.jpeg"} className="my-profile" alt="my profile" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
